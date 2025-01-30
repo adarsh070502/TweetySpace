549ae8b81da7da0938c353c1925155ba9a0e4f03
adarsh070502 <adarsheluri143@gmail.com>
2025-01-30 23:32:57 +0530

    Create the delete_follower api endpoint and sending updatedProfile on add and delete follower api endpoints

--- server/src/models/followersModel.js
+++ server/src/models/followersModel.js
@@ -1,14 +1,19 @@
 import mongoose from "mongoose";
 
-const followersSchema = new mongoose.Schema({
-  user_id: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "profileCollection",
-    required: true,
+const followersSchema = new mongoose.Schema(
+  {
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "profileCollection",
+      required: true,
+    },
+    followers_ids: [
+      { type: mongoose.Schema.Types.ObjectId, ref: "profileCollection" },
+    ],
   },
-  followers_ids: [
-    { type: mongoose.Schema.Types.ObjectId, ref: "profileCollection" },
-  ],
-});
+  {
+    timestamps: true,
+  }
+);
 
 export default mongoose.model("followerCollection", followersSchema);
--- server/src/routes/profile.js
+++ server/src/routes/profile.js
@@ -34,7 +34,7 @@ profileRouter.post("/add_follower", isAuthenticated, async (req, res) => {
   try {
     const { userId, followerId } = req.body;
 
-    // conevrting string userId, followerId into objectID
+    // converting string userId, followerId into objectID
     const userObjectId = new Types.ObjectId(userId);
     const followerObjectId = new Types.ObjectId(followerId);
 
@@ -58,7 +58,18 @@ profileRouter.post("/add_follower", isAuthenticated, async (req, res) => {
     // Add the follower and save the document
     followersDoc.followers_ids.push(followerObjectId);
     await followersDoc.save();
-    res.status(200).json({ message: "Follower added successfully" });
+
+    // Update the followers count in profile collection
+    const updatedProfile = await newProfile.findOneAndUpdate(
+      { UserId: userId }, // Find user by ID
+      { $inc: { FollowersCount: 1 } }, // Increment FollowersCount by 1
+      { new: true } // Return updated document
+    );
+
+    res.status(200).json({
+      message: "Follower added successfully",
+      updatedUserProfile: updatedProfile,
+    });
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Internal Server Error", error: err });
@@ -85,9 +96,60 @@ profileRouter.get("/get_followers", isAuthenticated, async (req, res) => {
       model: "profileCollection", // Model to populate from (profileCollection)
       foreignField: "UserId", // Field in profileCollection to match against
       localField: "followers_ids", // Field in Followers collection that stores the follower IDs
-      select: "UserName FullName", // Select the fields you want to populate
+      select: "UserName FullName Bio", // Select the fields you want to populate
     });
     res.status(200).json({ data: followersData });
+  } catch (err) {
+    res.status(500).json({ message: "Internal Server Error", error: err });
+  }
+});
+
+profileRouter.post("/delete_follower", isAuthenticated, async (req, res) => {
+  try {
+    const { userId, followerId } = req.body;
+
+    // converting string userId, followerId into objectID
+    const userObjectId = new Types.ObjectId(userId);
+    const followerObjectId = new Types.ObjectId(followerId);
+
+    // Extracting follower profile and checking if it exists
+    const followerProfile = await newProfile.findOne({ UserId: followerId });
+    if (!followerProfile) {
+      return res.status(404).json({ message: "Follower doesn't exist" });
+    }
+
+    // Fetching the followersDoc of the user from followersCollection
+    let followersDoc = await Followers.findOne({ user_id: userObjectId });
+    if (!followersDoc) {
+      return res.status(404).json({ message: "User doesn't exist" });
+    }
+
+    // Check if the follower is in the followers list
+    if (!followersDoc.followers_ids.includes(followerObjectId)) {
+      return res
+        .status(400)
+        .json({ message: "Follower is not following User" });
+    }
+
+    // Deleting the follower
+    followersDoc.followers_ids = followersDoc.followers_ids.filter(
+      (id) => id.toString() !== followerId
+    );
+
+    // Update the followers count in profile collection
+    const updatedProfile = await newProfile.findOneAndUpdate(
+      { UserId: userId }, // Find user by ID
+      { $inc: { FollowersCount: -1 } }, // Increment FollowersCount by 1
+      { new: true } // Return updated document
+    );
+
+    await followersDoc.save();
+    res
+      .status(200)
+      .json({
+        message: "Successfully unfollowed the user.",
+        updatedUserProfile: updatedProfile,
+      });
   } catch (err) {
     res.status(500).json({ message: "Internal Server Error", error: err });
   }
